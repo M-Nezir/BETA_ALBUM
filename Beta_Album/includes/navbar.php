24e42b0aaa436da9bb16b520fd48bad98cd818f9
M-Nezir <146748647+M-Nezir@users.noreply.github.com>
2025-01-28 14:57:30 +0300

    START

---
+++ Beta_Album/includes/navbar.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beta Album - Ana Sayfa</title>
+    <link rel="stylesheet" href="../css/navbar.css">
+</head>
+<body>
+
+<header>
+    <div style="padding:0px 0px;"></div>
+    <nav>
+        <ul>
+            <li><a href="#">Üye Giriş / Hesabım</a></li>
+            |
+            <li><a href="#">Hakkımızda</a></li>
+            |
+            <li><a href="#">İletişim</a></li>
+            |
+            <li><a href="#">Sepetim</a></li>
+            |
+            <li><a href="#">Kasaya git</a></li>
+        </ul>
+    </nav>
+</header>
+
+<nav>
+    <span style=""><b>BETA ALBUM&trade;</b></span>
+    <ul class="menu">
+        <li class="dropdown">
+            <a href="#">Panoramik Albüm ▽</a>
+            <div class="dropdown-content">
+                <div class="dropdown-item">
+                    <p>Bebek Albümleri</p>
+                </div>
+                <div class="dropdown-item">
+                    <p>Ekonomik Albümler</p>
+                </div>
+                <div class="dropdown-item">
+                    <p>Exclusive Albümler</p>
+                </div>
+                <div class="dropdown-item">
+                    <p>Kampanyalı Albümler</p>
+                </div>
+                <div class="dropdown-item">
+                    <p>Fırsat Albümleri</p>
+                </div>
+                <img src="https://fastly.picsum.photos/id/643/200/300.jpg?hmac=rS-MHa0BIMHdAgm-FZ7QM36aRKEAzzhSRNRv5n4uqGc" alt="Albüm Resmi">
+            </div>
+        </li>
+        <li class="dropdown">
+            <a href="#">Fotoğraf Baskı ▽</a>
+            <div class="dropdown-content">
+                <div class="dropdown-item">
+                    <p>Labartuvar Baskı</p>
+                </div>
+                <img src="https://fastly.picsum.photos/id/643/200/300.jpg?hmac=rS-MHa0BIMHdAgm-FZ7QM36aRKEAzzhSRNRv5n4uqGc" alt="Albüm Resmi">
+            </div>
+        </li>
+        <li class="dropdown">
+            <a href="#">Fotokitap ▽</a>
+            <div class="dropdown-content">
+                <div class="dropdown-item">
+                    <p>Fotokitap</p>
+                </div>
+                <img src="https://fastly.picsum.photos/id/643/200/300.jpg?hmac=rS-MHa0BIMHdAgm-FZ7QM36aRKEAzzhSRNRv5n4uqGc" alt="Albüm Resmi">
+            </div>
+        </li>
+        <li class="dropdown">
+            <a href="#">Fotoğraf Kutusu ▽</a>
+            <div class="dropdown-content">
+                <div class="dropdown-item">
+                    <p>Fotoğraf Kutusu</p>
+                </div>
+                <div class="dropdown-item">
+                    <p>Album Kutusu</p>
+                </div>
+                <img src="https://fastly.picsum.photos/id/643/200/300.jpg?hmac=rS-MHa0BIMHdAgm-FZ7QM36aRKEAzzhSRNRv5n4uqGc" alt="Albüm Resmi">
+            </div>
+        </li>
+        <li class="dropdown">
+            <a href="#">Duvar Sanatı ▽</a>
+            <div class="dropdown-content">
+                <div class="dropdown-item">
+                    <p>Fotoğraf Baskılı Çerçeve</p>
+                </div>
+                <div class="dropdown-item">
+                    <p>Foto Akordeon</p>
+                </div>
+                <div class="dropdown-item">
+                    <p>Kanvas Tablo</p>
+                </div>
+                <div class="dropdown-item">
+                    <p>Yapışkanlı Çerçeve</p>
+                </div>
+                <img src="https://fastly.picsum.photos/id/643/200/300.jpg?hmac=rS-MHa0BIMHdAgm-FZ7QM36aRKEAzzhSRNRv5n4uqGc" alt="Albüm Resmi">
+            </div>
+        </li>
+        <li class="dropdown">
+            <a href="#">Fotoğraflı Ürün ▽</a>
+            <div class="dropdown-content">
+                <div class="dropdown-item">
+                    <p>Mezuniyet Albümü</p>
+                </div>
+                <div class="dropdown-item">
+                    <p>Puzzle</p>
+                </div>
+                <div class="dropdown-item">
+                    <p>Bardak Baskı</p>
+                </div>
+                <img src="https://fastly.picsum.photos/id/643/200/300.jpg?hmac=rS-MHa0BIMHdAgm-FZ7QM36aRKEAzzhSRNRv5n4uqGc" alt="Albüm Resmi">
+            </div>
+        </li>
+        <li class="dropdown">
+            <a href="#">Photoshop Tasarım ▽</a>
+            <div class="dropdown-content">
+                <div class="dropdown-item">
+                    <p>Albüm Çalışması</p>
+                </div>
+                <div class="dropdown-item">
+                    <p>Tekli Poz</p>
+                </div>
+                <div class="dropdown-item">
+                    <p>Restore</p>
+                </div>
+                <img src="https://fastly.picsum.photos/id/643/200/300.jpg?hmac=rS-MHa0BIMHdAgm-FZ7QM36aRKEAzzhSRNRv5n4uqGc" alt="Albüm Resmi">
+            </div>
+        </li>
+    </ul>
+</nav>
+</body>
+</html>
