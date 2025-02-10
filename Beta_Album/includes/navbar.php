2fd5a78df4a01fb58020dfc4597e14ae1c9c1022
Mehmet Nezir Aydın <mehmetneziraydin20@gmail.com>
2025-02-10 05:33:32 +0300

    açıklama

--- Beta_Album/includes/navbar.php
+++ Beta_Album/includes/navbar.php
@@ -74,5 +74,182 @@ while ($row = mysqli_fetch_assoc($result)) {
     </ul>
 </nav>
 
+<div class="hamburger-button">
+    <a href="/BETA_ALBUM/Beta_Album/index.php"  class="icon_mobile left"><img src="/BETA_ALBUM/Beta_Album/image/betaalbümyatay.svg" alt="Logo" width="140" height=auto></a>
+    <a class="search_icon_mobile"href=""><i class="fas fa-search"></i></a>
+    <a class="search_icon_mobile" href="/BETA_ALBUM/Beta_Album/includes/login.php"><i class="fas fa-user"></i></a>
+    <a class="search_icon_mobile" href="/BETA_ALBUM/Beta_Album/includes/basket.php"><i class="fa-solid fa-basket-shopping"></i></a>
+<button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu" aria-controls="offcanvasMenu">
+      ☰
+    </button>
+</div>
+
+
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasMenu" aria-labelledby="offcanvasMenuLabel">
+      <div class="offcanvas-header" style="background-color: #f4e3d4; height:8%;">
+      <span class="icon_mobile"><b>BETA ALBUM&trade;</b></span>
+      </div>
+
+      <div class="offcanvas-body p-0">
+        <div class="list-group" id="menuList">
+          <!-- Dropdown açılır liste 1 -->
+          <a href="#submenu1" class="list-group-item list-group-item-action menu-item d-flex justify-content-between align-items-center dropdown-toggle">
+            <b>Panoramik Albüm </b>
+          </a>
+          <div class="collapse" id="submenu1">
+            <div class="list-group">
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Bebek Albümleri" class="list-group-item list-group-item-action menu-item">Bebek Albümleri</a>
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Ekonomik Albümler" class="list-group-item list-group-item-action menu-item">Ekonomik Albümler</a>
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Exclusive Albümler" class="list-group-item list-group-item-action menu-item">Exclusive Albümler</a>
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Kampanyalı Albümler" class="list-group-item list-group-item-action menu-item">Kampanyalı Albümler</a>
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Fırsat Albümleri" class="list-group-item list-group-item-action menu-item">Fırsat Albümleri</a>
+            </div>
+          </div>
+
+          <!-- Dropdown açılır liste 2 -->
+          <a href="#submenu2" class="list-group-item list-group-item-action menu-item d-flex justify-content-between align-items-center dropdown-toggle">
+          <b>Fotoğraf Baskı</b>
+          </a>
+          <div class="collapse" id="submenu2">
+            <div class="list-group">
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Laboratuvar Baskı" class="list-group-item list-group-item-action menu-item">Laboratuvar Baskı</a>
+            </div>
+          </div>
+
+          <!-- Dropdown açılır liste 2 -->
+          <a href="#submenu3" class="list-group-item list-group-item-action menu-item d-flex justify-content-between align-items-center dropdown-toggle">
+          <b>Fotokitap</b>
+          </a>
+          <div class="collapse" id="submenu3">
+            <div class="list-group">
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Fotokitap" class="list-group-item list-group-item-action menu-item">Fotokitap</a>
+            </div>
+          </div>
+
+          <!-- Dropdown açılır liste 2 -->
+          <a href="#submenu4" class="list-group-item list-group-item-action menu-item d-flex justify-content-between align-items-center dropdown-toggle">
+          <b>Fotoğraf Kutusu</b>
+          </a>
+          <div class="collapse" id="submenu4">
+            <div class="list-group">
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Fotoğraf Kutusu" class="list-group-item list-group-item-action menu-item">Fotoğraf Kutusu</a>
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Album Kutusu" class="list-group-item list-group-item-action menu-item">Album Kutusu</a>
+            </div>
+          </div>
+
+          <!-- Dropdown açılır liste 2 -->
+          <a href="#submenu5" class="list-group-item list-group-item-action menu-item d-flex justify-content-between align-items-center dropdown-toggle">
+          <b>Duvar Sanatı</b>
+          </a>
+          <div class="collapse" id="submenu5">
+            <div class="list-group">
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Fotoğraf Baskılı Çerçeve" class="list-group-item list-group-item-action menu-item">Fotoğraf Baskılı Çerçeve</a>
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Foto Akordeon" class="list-group-item list-group-item-action menu-item">Foto Akordeon</a>
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Kanvas Tablo" class="list-group-item list-group-item-action menu-item">Kanvas Tablo</a>
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Yapışkanlı Çerçeve" class="list-group-item list-group-item-action menu-item">Yapışkanlı Çerçeve</a>
+            </div>
+          </div>
+
+          <!-- Dropdown açılır liste 2 -->
+          <a href="#submenu6" class="list-group-item list-group-item-action menu-item d-flex justify-content-between align-items-center dropdown-toggle">
+          <b>Fotoğraflı Ürün</b>
+          </a>
+          <div class="collapse" id="submenu6">
+            <div class="list-group">
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Mezuniyet Albümü" class="list-group-item list-group-item-action menu-item">Mezuniyet Albümü</a>
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Puzzle" class="list-group-item list-group-item-action menu-item">Puzzle</a>
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Bardak Baskı" class="list-group-item list-group-item-action menu-item">Bardak Baskı</a>
+            </div>
+          </div>
+
+          <!-- Dropdown açılır liste 2 -->
+          <a href="#submenu7" class="list-group-item list-group-item-action menu-item d-flex justify-content-between align-items-center dropdown-toggle">
+          <b>Photoshop Tasarım</b>
+          </a>
+          <div class="collapse" id="submenu7">
+            <div class="list-group">
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Albüm Çalışması" class="list-group-item list-group-item-action menu-item">Albüm Çalışması</a>
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Tekli Poz" class="list-group-item list-group-item-action menu-item">Tekli Poz</a>
+              <a href="/BETA_ALBUM/Beta_Album/includes/products.php?Kategori=Restore" class="list-group-item list-group-item-action menu-item">Restore</a>
+            </div>
+        </div>
+        <div class="single-items">
+                <a href="/BETA_ALBUM/Beta_Album/includes/login" class="list-group-item list-group-item-action menu-item">Üye Giriş / Hesabım</a>
+                <a href="/BETA_ALBUM/Beta_Album/includes/about_us" class="list-group-item list-group-item-action menu-item">Hakkımızda</a>
+                <a href="/BETA_ALBUM/Beta_Album/includes/contact" class="list-group-item list-group-item-action menu-item">İletişim</a>
+                <a href="/BETA_ALBUM/Beta_Album/includes/basket" class="list-group-item list-group-item-action menu-item">Sepetim</a>
+                <a href="/BETA_ALBUM/Beta_Album/includes/basket" class="list-group-item list-group-item-action menu-item">Kasaya git</a>
+        
+            </div>
+      </div>
+    </div>
+</div>
+
+   
+    <script>
+
+      // İşlev: tüm dropdown'ları kapat
+      function closeAllDropdowns(except = null) {
+        document.querySelectorAll('.dropdown-toggle').forEach(toggle => {
+          const targetSelector = toggle.getAttribute('href');
+          const targetCollapse = document.querySelector(targetSelector);
+          if (targetCollapse.classList.contains('show') && toggle !== except) {
+            let instance = bootstrap.Collapse.getInstance(targetCollapse);
+            if (!instance) {
+              instance = new bootstrap.Collapse(targetCollapse, { toggle: false });
+            }
+            instance.hide();
+            toggle.classList.remove('active');
+          }
+        });
+      }
+
+      // Normal menü öğeleri aktiflik ayarı (dropdown olmayanlar)
+      const menuItems = document.querySelectorAll('.menu-item:not(.dropdown-toggle)');
+      menuItems.forEach(item => {
+        item.addEventListener('click', function () {
+          // Tüm menü öğelerindeki aktif sınıfı kaldır
+          document.querySelectorAll('.menu-item').forEach(i => i.classList.remove('active'));
+          this.classList.add('active');
+        });
+      });
+
+      // Dropdown toggle öğeleri için davranış
+      const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+      dropdownToggles.forEach(toggle => {
+        toggle.addEventListener('click', function (e) {
+          e.preventDefault(); // Linkin varsayılan davranışını iptal et
+          const targetSelector = this.getAttribute('href');
+          const targetCollapse = document.querySelector(targetSelector);
+
+          // Bootstrap Collapse instance'ını al veya oluştur
+          let bsCollapse = bootstrap.Collapse.getInstance(targetCollapse);
+          if (!bsCollapse) {
+            bsCollapse = new bootstrap.Collapse(targetCollapse, { toggle: false });
+          }
+
+          // Eğer tıklanan dropdown açıksa, kapat; değilse diğerlerini kapatıp aç
+          if (targetCollapse.classList.contains('show')) {
+            bsCollapse.hide();
+            this.classList.remove('active');
+          } else {
+            closeAllDropdowns(this);
+            bsCollapse.show();
+            this.classList.add('active');
+          }
+        });
+      });
+    </script>
+
+<script>
+        window.onscroll = function() {
+            if (window.scrollY > 0) {
+                document.body.classList.add("scrolled");
+            } else {
+                document.body.classList.remove("scrolled");
+            }
+        };
+    </script>
+
 </body>
 </html>
